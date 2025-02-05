7c4502cd12099101541249511186a131619d8488
HenryW7175 <143651504+HenryWu7175@users.noreply.github.com>
2025-02-05 15:47:59 -0500

    i added an error message by coding a if statement to check if the name textview is empty or not. the second issue is resolved

--- app/src/main/java/edu/temple/helloworld/MainActivity.kt
+++ app/src/main/java/edu/temple/helloworld/MainActivity.kt
@@ -10,6 +10,7 @@ class MainActivity : AppCompatActivity() {
 
     // Declare view properties - the first one is done for you
     lateinit var displayTextView: TextView
+    lateinit var nametext: TextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -18,10 +19,14 @@ class MainActivity : AppCompatActivity() {
         // Initialize with views defined in Layout - the first one is done for you
         displayTextView = findViewById(R.id.displayTextView)//var is initialized for the display text view
         // , the one that we have to change to the user name entry later on
-
-        
+        nametext = findViewById(R.id.nameEditText)
         findViewById<Button>(R.id.clickMeButton).setOnClickListener {
-            displayTextView.text = "Hello, ${findViewById<EditText>(R.id.nameEditText).text}"
+            if(nametext.text.isEmpty()) {
+                displayTextView.text = "Please enter your name."
+            }else{
+                displayTextView.text = "Hello, ${nametext.text}"
+            }
+
         }
 
 
